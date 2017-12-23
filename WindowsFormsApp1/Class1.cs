9dfc09be1d231a9364f21715b5ab651adbeae4da
Leo <279598616@qq.com>
2017-12-24 00:09:12 +0800

    用于测试C#其他类调用控件并修改其属性

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public class Class1
+    {
+       static public void test()
+        {
+            Form1.form.textBox1.Text = "修改成功";
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 form;
+        public Form1()
+        {
+            InitializeComponent();
+            form = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1.test();
+        }
+    }
+}
